e658fc9efe32c5651369944a1a642f8e5e7c18c0
Scribble <scribble@minecrafttas.com>
2023-05-11 16:04:47 +0200

    Added template

---
+++ 1.14.4+/src/main/java/com/minecrafttas/disco_template/ModContainer.java
@@ -0,0 +1,53 @@
+package com.minecrafttas.disco_template;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ModContainer implements ModInitializer{
+
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Disco World!");
+		
+		//////////////////////////////////
+		// To do version specific code: //
+		//////////////////////////////////
+		
+		//#1.16.1
+//$$		System.out.println("Only enabled in 1.16.1 and above");
+		//#end
+		
+		
+		////////////////////////////////////////
+		// You can add multiple versions too: //
+		////////////////////////////////////////
+		
+		//# 1.15.2
+//$$		System.out.println("Only enabled in 1.15.2 and above");
+		//# 1.14.4
+//$$		System.out.println("Only enabled in 1.14.4 and above");
+		//# end
+		
+		
+		////////////////////////////////////////
+		// Def is always the lowest version: //
+		///////////////////////////////////////
+		
+		//# 1.18.2
+//$$		System.out.println("Only enabled in 1.18.2 and above");
+		//# def
+//$$		System.out.println("Only enabled in 1.14.4 and above");
+		//# end
+		
+		
+		////////////////////////////////////////
+		// 		Ordering is arbitrary! 		 //
+		///////////////////////////////////////
+		
+		//# def
+//$$		System.out.println("Only enabled in 1.14.4 and above");
+		//# 1.18.2
+//$$		System.out.println("Only enabled in 1.18.2 and above");
+		//# end
+	}
+
+}
---
+++ 1.14.4+/src/main/java/com/minecrafttas/disco_template/mixin/MixinMinceraft.java
@@ -0,0 +1,37 @@
+package com.minecrafttas.disco_template.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+//#1.19.4
+//$$@Mixin(net.minecraft.client.gui.components.LogoRenderer.class)
+//#def
+//$$@Mixin(net.minecraft.client.gui.screens.TitleScreen.class)
+//#end
+public class MixinMinceraft {
+	
+	@Shadow
+	@Mutable
+	//#1.19.4
+//$$	private boolean showEasterEgg;
+	//#def
+//$$	private boolean minceraftEasterEgg;
+	//#end
+	
+	//#1.19.4
+//$$	@Inject(method = "renderLogo", at = @At("HEAD"))
+	//#def
+//$$	@Inject(method = "init", at = @At("HEAD"))
+	//#end
+	public void inject_init(CallbackInfo ci) {
+		//#1.19.4
+//$$		showEasterEgg = true;
+		//#def
+//$$		minceraftEasterEgg = true;
+		//#end
+	}
+}
